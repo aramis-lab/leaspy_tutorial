af05d9b8bb68ca7943ddd603bbba62b58b7cedb2
Sebastian <mendezpsebastian@gmail.com>
2026-08-04 16:50:58 +0200

    adding canonical data, new way to plot and minor fixes

--- content/solutions/q1.py
+++ content/solutions/q1.py
@@ -4,6 +4,6 @@ model_1_source = LogisticModel(name="logistic", source_dimension=1)
 model_1_source.fit(
     df_train, "mcmc_saem",
     seed=SEED, n_iter=1000, progress_bar=True,
-    save_periodicity=500, plot_periodicity=500,
+    save_periodicity=500,
     overwrite_logs_folder=True,
 )

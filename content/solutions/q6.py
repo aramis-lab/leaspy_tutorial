aa70b783dc6915540b621b1f370baf15583498ad
Jean MARTINI <jv.martini@icm-institute.org>
2026-08-05 15:30:54 +0200

    small fixes and precisions

--- content/solutions/q6.py
+++ content/solutions/q6.py
@@ -1,5 +1,5 @@
 # Generate a new cohort from the fitted model and the visit design
-simulated = model.simulate(
+simulated = model_100k.simulate(
     algorithm="simulate",
     features=FEATURES,
     visit_parameters=visit_parameters,
